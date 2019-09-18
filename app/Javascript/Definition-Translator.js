fb448f8aea666a108fae94f6217bf7be0dbd520e
BrandonMGH <BrandonMGH@Yahoo.com>
2019-09-18 15:00:37 -0700

    connected HTML file to Definiiton js filed and created a basic function that grabbed the value from the HTML input form field

---
+++ app/Javascript/Definition-Translator.js
@@ -0,0 +1,11 @@
+
+
+
+function getcube(){  
+    var number=document.getElementById("number").value; 
+    alert(number*number*number);  
+}
+
+// function inputValue (){
+//     console.log(name_element.value)
+// }
