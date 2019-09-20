5e104fbea8e39a64cd6e264228a328d4e8d65204
BrandonMGH <BrandonMGH@Yahoo.com>
2019-09-20 13:50:03 -0700

    got rid of Github theming file and cleared out ReadMe.md

--- app/Javascript/Definition-Translator.js
+++ app/Javascript/Definition-Translator.js
@@ -1,30 +1,32 @@
 require('dotenv').config()
 const axios = require("axios");
 
+
+
 const apiKey = process.env.Merriam_API_Key
-var wordValue = 'television'
+let wordValue;
 const apiLink = `https://dictionaryapi.com/api/v3/references/collegiate/json/${wordValue}?key=${apiKey}`
 
 
 function getWordValue () {
-  document.getElementById('test').innerHTML=document.getElementById("wordInsert").value;
+  wordValue=document.getElementById("wordInsert").value;
+  defineWord();
 }
  
 
 
 
-
 function defineWord () {
   console.log(wordValue)
-    axios.get(apiLink)
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
+    // axios.get(apiLink)
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response.data);
+    // })
+    // .catch(function (error) {
+    //   // handle error
+    //   console.log(error);
+    // })
   }
 
 
